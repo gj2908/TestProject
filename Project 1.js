9dd6e1f02cd0d351d3d6a3105cc0fce08485a21f
Gaurang Jadoun <gaurangjadoun@gmail.com>
2025-12-17 17:04:24 +0530

    Button.js

--- Project 1.js
+++
@@ -1,7 +0,0 @@
-// Change webpage content dynamically
-document.getElementById("demo").innerHTML = "Hello, JavaScript!";
-
-// Handle a button click
-document.querySelector("button").addEventListener("click", () => {
-  alert("Button clicked!");
-});
