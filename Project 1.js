0607ced0b25fa247ed5fba79dc223922b4f875d7
Gaurang Jadoun <gaurangjadoun@gmail.com>
2025-12-17 12:46:15 +0530

    Initial Commit

---
+++ Project 1.js
@@ -0,0 +1,7 @@
+// Change webpage content dynamically
+document.getElementById("demo").innerHTML = "Hello, JavaScript!";
+
+// Handle a button click
+document.querySelector("button").addEventListener("click", () => {
+  alert("Button clicked!");
+});
